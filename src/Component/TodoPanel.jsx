b715ff45f53ef2e0258b50cf1fd75957b410eb2f
Jawad Chodhri <152736441+jawadchodhri@users.noreply.github.com>
2026-07-01 19:20:43 +0500

    Update TodoPanel.jsx

--- src/Component/TodoPanel.jsx
+++ src/Component/TodoPanel.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function TodoPanel(){
     return <div>
         <main className="flex justify-center px-10 py-10">
-            <section className="w-full max-w-xlnp rounded-lg bg-gray-600 p-6 shadow-lg">
+            <section className="w-full max-w-xl rounded-lg bg-gray-600 p-6 shadow-lg">
                 <h2 className="mb-4 text-xl font-semibold text-white">My Tasks</h2>{/*input*/}</section>
         </main>
     </div>

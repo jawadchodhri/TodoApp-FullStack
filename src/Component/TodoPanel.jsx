7fc4fb2e4b20e79c3a69132107d8b2083a8a35be
Jawad Chodhri <152736441+jawadchodhri@users.noreply.github.com>
2026-07-02 15:47:52 +0500

    Update TodoPanel.jsx

--- src/Component/TodoPanel.jsx
+++ src/Component/TodoPanel.jsx
@@ -8,6 +8,7 @@ function TodoPanel(){
     function addTask(){
         if (task === "") return;
 
+
         const updatedTasks = [...totalTasks, task];
 
         setTotalTasks(updatedTasks);
@@ -15,24 +16,30 @@ function TodoPanel(){
         setTask("");
     }
 
+    function removeTask(indexToRemove){
+        const updatedTask = totalTasks.filter((item, index) => index !== indexToRemove)
+
+        setTotalTasks(updatedTask)
+    }
+
     return <div>
         <main className="flex justify-center px-10 py-10">
             <section className="w-full max-w-xl rounded-lg bg-gray-600 p-6 shadow-lg">
-                <h2 className="mb-4 text-xl font-semibold text-white">My Tasks</h2>{/*input*/}
+                <h2 className="mb-4 text-xl font-semibold text-white">My Tasks</h2>
                 <div className="flex gap-2">
                     <input type="text"
-                            // value = {task}
+                            value = {task}
                             onChange = {(userInput) => setTask(userInput.target.value)}
                             placeholder="Enter a task"
                             className="flex-1 rounded-md px-3 py-2 text-black"
                     />
                     <button onClick={addTask} className="rounded-md bg-blue-500 px-6 py-2 text-white">Add</button>
-                    {/* <button className="rounded-md bg-green-500 px-6 py-2 text-white">Completed</button> */}
                 </div>
                 <ul className="mt-4 space-y-2 text-left">
                     {totalTasks.map((item, index) => (
-                        <li key = {index} className="rounded-md bg-gray-700 px-3 py-2 text-white">
-                            {item}
+                        <li key = {index} className="flex item-center justify-between rounded-md bg-gray-700 px-3 py-2 text-white">
+                            <span>{item}</span>
+                            <button onClick= {() => removeTask(index)} className="rounded-md bg-green-500 px-6 py-2 text-white">Completed</button>
                         </li>
                     ))}
                 </ul>
